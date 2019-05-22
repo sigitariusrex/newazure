80c34129d4c158f60bec86808e0c47ecef5d5b53
sigitariusrex <47717829+sigitariusrex@users.noreply.github.com>
2019-05-22 11:48:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
 	
 if (isset($_POST['submit'])) {
 	
-	$containerName = "newazure2";
+	$containerName = "newazure".generateRandomString();
 	
 	$fileToUpload = $_FILES["fileToUpload"]["name"];
 	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
